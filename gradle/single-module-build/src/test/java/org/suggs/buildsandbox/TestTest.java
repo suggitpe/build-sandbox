55100b3927d98f11548f0a07013d9d92b104d80f
Peter Suggitt <me@suggs.org.uk>
2012-12-20 14:31:35 +0000

    Added a test to the single module gradle projec

---
+++ gradle/single-module-build/src/test/java/org/suggs/buildsandbox/TestTest.java
@@ -0,0 +1,12 @@
+package org.suggs.buildsandbox;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class TestTest {
+
+    @Test
+    public void testMethod() {
+        assertTrue(true);
+    }
+}
