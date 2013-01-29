4c9ab8ac554e4488076c9e63ec97ad9b5925c64b
Peter Suggitt <me@suggs.org.uk>
2013-01-29 08:52:17 +0000

    Updated for multi project build

--- sbt/multi-module-build/project/Build.scala
+++ sbt/multi-module-build/project/Build.scala
@@ -2,5 +2,34 @@ import sbt._
 import sbt.Keys._
 
 object Build extends Build {
+
+    lazy val root = Project(
+        id = "SbtOpenspace",
+        base = file(".")
+    ) aggregate(rdsApi, rdsImpl)
+
+    lazy val rdsApi = Project(
+        id = "rds-api",
+        base = file("rds-api"),
+        dependencies = Seq()
+    )
+
+    lazy val rdsImpl = Project(
+        id = "rds-impl",
+        base = file("rds-impl"),
+        dependencies = Seq(rdsApi)
+    )
+
+    override lazy val settings =
+        super.settings ++
+            buildSettings ++
+            Seq( shellPrompt := {s => Project.extract(s).currentProject.id + "> " } )
+
+    lazy val buildSettings = Seq(
+        organization := "org.suggs",
+        version := "1.0",
+        scalaVersion := "2.10.0"
+    )
+
 }
 
