1eb1e6fc433ab6f1bc55271ba4c694a504a86ec3
Peter Suggitt <me@suggs.org.uk>
2013-02-04 07:27:06 +0000

    Tidied up the gradle build mechanism

---
+++ gradle/multi-module-build/rds-api/src/main/java/AdderUpper.java
@@ -0,0 +1,11 @@
+/**
+ * dumb ass class to be called by the impl class
+ */
+public interface AdderUpper {
+
+    /**
+     * Adds two ints together
+     */
+    int add(int first, int second);
+
+}
---
+++ gradle/multi-module-build/rds-impl/src/main/java/AdderUpperImpl.java
@@ -0,0 +1,10 @@
+/**
+ * Adds two ints together
+ */
+public class AdderUpperImpl implements AdderUpper {
+
+    @Override
+    public int add(int first, int second) {
+        return first + second;
+    }
+}
